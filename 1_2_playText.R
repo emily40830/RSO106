fb55869728fe1fb4781cd3fb3789caf16ba0926a
jirlong <jirlong@gmail.com>
2018-03-12 01:38:19 +0800

    mbp

---
+++ 1_2_playText.R
@@ -0,0 +1,102 @@
+library(tidyverse)
+# https://www.tidytextmining.com/tidytext.html
+
+
+
+# Loading data ------------------------------------------------------------
+
+load(url("http://varianceexplained.org/files/trump_tweets_df.rda"))
+View(trump_tweets_df)
+names(trump_tweets_df)
+# [1] "text"          "favorited"     "favoriteCount" "replyToSN"
+# [5] "created"       "truncated"     "replyToSID"    "id"
+# [9] "replyToUID"    "statusSource"  "screenName"    "retweetCount"
+# [13] "isRetweet"     "retweeted"     "longitude"     "latitude"
+
+tweets <- trump_tweets_df
+
+
+
+
+# Inspecting the text -----------------------------------------------------
+
+tweets$text
+length(tweets$text)
+nchar(tweets$text)
+hist(nchar(tweets$text))
+?hist
+hist(nchar(tweets$text), breaks = 20)
+
+
+
+# Seperating sentences to words -------------------------------------------
+# https://www.tidytextmining.com/tidytext.html
+??unnest_tokens
+
+library(tidytext)
+unnest.df <- unnest_tokens(tweets, word, text, drop = FALSE)
+unnest.df <- unnest_tokens(tweets, word, text, to_lower = TRUE)
+
+
+
+# Removing stop words -----------------------------------------------------
+
+data(stop_words)
+stop.df <- anti_join(unnest.df, stop_words)
+
+
+
+
+# Counting word frequency -------------------------------------------------
+
+count(stop.df, word, sort = TRUE)
+
+
+
+# Re-do it by dplyr -------------------------------------------------------
+
+word_freq <- tweets %>%
+    unnest_tokens(word, text, drop = FALSE) %>%
+    anti_join(stop_words) %>%
+    count(word, sort = TRUE)
+
+
+
+# Visualizing words sorted by frequnecy -----------------------------------
+
+unnested <- tweets %>%
+    unnest_tokens(word, text, drop = FALSE) %>%
+    anti_join(stop_words)
+
+
+unnested %>%
+    count(word, sort = TRUE) %>%
+    top_n(50) %>%
+    mutate(word = reorder(word, n)) %>%
+    ggplot(aes(word, n)) +
+    geom_col() +
+    coord_flip()
+
+
+
+
+# Toward Chinese text processing ------------------------------------------
+
+library(stringr)
+
+tweets %>%
+    mutate(word = map(text, function(x)unlist(str_split(x, "\\s")))) %>%
+    unnest(word)
+
+
+
+# Assignment --------------------------------------------------------------
+
+library(jsonlite)
+library(httr)
+url <- "https://www.dcard.tw/_api/forums/relationship/posts?popular=true"
+res <- fromJSON(content(GET(url), "text"))
+
+
+
+
